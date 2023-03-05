eed3368b801daab6af201372f5123298358b7d35
LIEW, Wai Yip <liewwy19@gmail.com>
2023-03-05 09:49:53 +0000

    instrument Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,12 @@ import watchtower
 import logging
 from time import strftime
 
+# ROLLBAR
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # CloudWatch Logs: Configuring Logger to Use CloudWatch
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
@@ -45,6 +51,9 @@ provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
+# ROLLBAR
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+
 # X-RAY
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
@@ -57,6 +66,22 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 # X-RAY
 XRayMiddleware(app, xray_recorder)
@@ -81,6 +106,11 @@ def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
