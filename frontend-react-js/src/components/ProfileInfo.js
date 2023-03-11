9294dc5728625ebd3ec3742e2ef53521951f3499
LIEW, Wai Yip <liewwy19@gmail.com>
2023-03-11 16:44:29 +0000

    implement backend check for the token

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -29,6 +29,7 @@ export default function ProfileInfo(props) {
     try {
         await Auth.signOut({ global: true });
         window.location.href = "/"
+        localStorage.removeItem("access_token")
     } catch (error) {
         console.log('error signing out: ', error);
     }
